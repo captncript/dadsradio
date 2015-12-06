6f1b8b63ac524e183a91c76b435332a544298594
Nick <Nick>
2015-12-06 10:46:53 -0500

    Testing

--- dadsRadio/app/src/main/java/com/captncript/dadsRadio/MainActivity.java
+++ dadsRadio/app/src/main/java/com/captncript/dadsRadio/MainActivity.java
@@ -1,7 +1,13 @@
 package com.captncript.dadsRadio;
 
 import android.app.*;
+import android.media.MediaPlayer;
 import android.os.*;
+
+import java.io.IOException;
+import java.io.File;
+import java.io.PrintStream;
+import java.io.FileNotFoundException;
 
 public class MainActivity extends Activity 
 {
@@ -10,5 +16,28 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        outputSetup();
+    }
+
+    protected void outputSetup() {
+
+        try {
+            File file = new File("./error");
+            PrintStream ps = new PrintStream(file);
+
+            System.setOut(ps);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void sound() {
+        MediaPlayer mp = new MediaPlayer();
+        try {
+            mp.setDataSource("./song.mp3");
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
