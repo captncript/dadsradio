ca20303931ee5d43cb036b20825530e867446896
captncript <nickarn99@gmail.com>
2015-12-22 19:34:11 -0500

    New Layout for a dialog.
    Added more radio content

--- dadsRadio/app/build/gen/com/captncript/dadsRadio/R.java
+++ dadsRadio/app/build/gen/com/captncript/dadsRadio/R.java
@@ -14,6 +14,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int Cancel=0x7f060006;
+        public static final int OK=0x7f060005;
         public static final int button3=0x7f060003;
         public static final int display=0x7f060000;
         public static final int pause=0x7f060002;
@@ -22,6 +24,7 @@ public final class R {
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int songpicker=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
